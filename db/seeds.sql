d49384b1c87186bccec3525d46a57714797136da
ense <eddi.n.esteban@gmail.com>
2020-06-25 20:35:17 -0400

    testing gitignore

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers VALUES(0, 'Golden Goat Burger', FALSE);
+INSERT INTO burgers VALUE(0, 'Brave Beef Burger', FALSE);
+INSERT INTO burgers VALUE(0, 'Charming Chicken Burger', FALSE);
+
